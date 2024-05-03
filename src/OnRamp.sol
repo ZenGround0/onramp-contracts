53b5848a4dc14ffdd6c25dc4528369fe0b98755d
zenground0 <ZenGround0@users.noreply.github.com>
2024-05-03 00:01:08 -0600

    mostly done with l2

--- src/OnRamp.sol
+++ src/OnRamp.sol
@@ -1,22 +1,29 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.17;
 
-import "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {IERC20} from '../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol';
+import {ProofData} from '../lib/raas-starter-kit/contracts/data-segment/ProofTypes.sol';
+import {verify} from '../lib/raas-starter-kit/contracts/data-segment/Proof.sol';
+import {cidToPieceCommitment} from '../lib/raas-starter-kit/contracts/data-segment/Cid.sol';
+
 
 interface OnRamp {
     struct Deal {
-        bytes32 commP;
-        int64 storage_duration;
+        bytes commP;
+        int64 duration;
         string location;
         uint256 amount;
         IERC20 token;
     }
+    // struct Hint {string location, uint64 size} ? 
+    // struct Payment {uint256 amount, IERC20 token}?
+
 
     event DataReady(Deal deal, uint64 id);
 
-    function offer_data(Deal calldata deal) external payable returns (DealID);
-    function verify_data_stored(uint64 id) external returns (bool);
-    function prove_data_stored(uint64 id, bytes calldata proof) external;
+    function offerData(Deal calldata deal) external payable returns (DealID);
+    function verifyDataStored(uint64 id) external returns (bool);
+    function proveDataStored(uint64 id, bytes calldata proof) external;
 }
 
 contract OnRampContract is OnRamp {
@@ -24,9 +31,11 @@ contract OnRampContract is OnRamp {
     uint64 private nextAggregateID = 1;
     mapping(uint64 => Deal) public deals;
     mapping(uint64 => uint64[]) public aggregations;
+    mapping(uint64 => address) public aggregationPayout;
+    mapping(uint64 => bool) public provenAggregations;
 
     function offer_data(Deal calldata deal) external payable override returns (uint64) {
-        require(deal.token.transferFrom(msg.sender, address(this), deal.amount), "Payment transfer failed");
+        require(deal.token.transferFrom(msg.sender, address(this), deal.amount), 'Payment transfer failed');
 
         uint64 id = nextDealId++;
         deals[id] = deal;
@@ -35,30 +44,41 @@ contract OnRampContract is OnRamp {
         return id;
     }
 
-    function commit_aggregate(commP aggregate, uint64[] deals, PODSI[] deal_proofs, address payout_addr) external override {
-        // check that the proofs are valid for each deal
+    // TODO: duration bounds checking 
+    function commitAggregate(bytes aggregate, uint64[] claimedIDs, ProofData[] inclusionProofs, address payoutAddr) external override {
+        uint64[] memory dealIDs = new uint64[](claimedIDs.length);
+        // Prove all deals are committed by aggregate commP 
+        for (uint64 i = 0; i < claimedIDs.length; i++) {
+            uint64 dealID = claimedIDs[i];
+            dealIDs[i] = dealID;
+            require(verify(cidToPieceCommitment(deals[dealID].commP), aggregate, inclusionProofs[i]), 'Proof verification failed');
 
-        // create the aggregate
+        }
+        aggregations[nextAggregateID] = dealIDs;
+        aggregationPayout[nextAggregateID] = payoutAddr;
 
         // call into axelar bridge targeting our filecoin prover contracts
         // passing in aggregateID and commP 
+        // For tomorrow just call to the proving contract
     }
 
-    function verify_data_stored(uint64 aggID, uint64 dealID) external override returns (bool) {
-
-        // check agg proven
-
-        // check agg refers to deal id
+    function verifyDataStored(uint64 aggID, uint idx, uint64 dealID) external override returns (bool) {
+        require(provenAggregations[aggID], 'Provided aggregation not proven');
+        require(aggregations[aggID][idx] == dealID, 'Aggregation does not include deal');
 
         return true;
     }
 
     // probably needs to be wrapped in an axelar _execute function
-    function prove_data_stored(uint64 aggID, bytes calldata proof) external override {
+    function proveDataStored(uint64 aggID) external override {
         // check that the caller is one of our trusted filecoin data prover contracts 
+        // TODO methods to add trusted callers 
 
-        // transfer payment to the receiver 
+        // transfer payment to the receiver
+        require(aggregationPayout[aggID].transfer(payment), 'Payment transfer failed');   
 
         // mark agg proven 
+        provenAggregations[aggID] = true;
     }
 }
+
