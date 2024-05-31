1b62d435df7a1a3478cb3b72fa815da9139f15a8
zenground0 <ZenGround0@users.noreply.github.com>
2024-05-31 14:44:41 -0600

    Fix address parsing

--- contract-tools/deploy-onramp.fish
+++ contract-tools/deploy-onramp.fish
@@ -56,16 +56,16 @@ function deploy-onramp
 
 	cd $LOTUS_EXEC_PATH
 	# Parse address from eth keystore file 
-	set filClientAddr (cat $XCHAIN_KEY_PATH | jq '.address' | sed -e 's/\"//g')
-	(./lotus evm stat $filClientAddr)
+	set clientAddr (cat $XCHAIN_KEY_PATH | jq '.address' | sed -e 's/\"//g')
+	set filClientAddr (parse-filecoin-addr (./lotus evm stat $clientAddr))
 
-	./lotus state wait-msg --timeout "2m" (./lotus send (parse-filecoin-address $filClientAddr) 10000)
+	./lotus state wait-msg --timeout "2m" (./lotus send $filClientAddr 10000)
 	cd $ONRAMP_CODE_PATH
 	jq -c '.abi' out/OnRamp.sol/OnRampContract.json > ~/.xchain/onramp-abi.json
 
-	jo -a (jo -- ChainID=314 Api="ws://localhost:1234/rpc/v1" -s OnRampAddress="$onrampAddr" KeyPath="$XCHAIN_KEY_PATH" ClientAddr="$filClientAddr" OnRampABIPath=~/.xchain/onramp-abi.json) > ~/.xchain/config.json
+	jo -a (jo -- ChainID=314 Api="ws://localhost:1234/rpc/v1" -s OnRampAddress="$onrampAddr" KeyPath="$XCHAIN_KEY_PATH" ClientAddr="$clientAddr" OnRampABIPath=~/.xchain/onramp-abi.json) > ~/.xchain/config.json
 	echo "config written to ~/.xchain/config.json" 
-	deploy-tokens $filClientAddr
+	deploy-tokens $clientAddr
 end
 
 #  $argv[1] path to compiled file
